30126fd55ebcaba899f755002b45b3c4c9afd9f2
Gerardo <gerardoguevara91@outlook.com>
2019-07-09 20:29:55 -0600

    Completa Funcion Presta y prestamo para cliente nuevo

--- src/connectionsDB/Operaciones.java
+++ src/connectionsDB/Operaciones.java
@@ -8,8 +8,10 @@ import java.util.Scanner;
  * @author Gerardo
  */
 public class Operaciones extends AccessDB {
-   Scanner SC = new Scanner(System.in);
-   int raw = 0;
+
+    Scanner SC = new Scanner(System.in);
+    int raw = 0;
+
     @Override
     protected void createConection() {
         super.createConection();
@@ -25,23 +27,22 @@ public class Operaciones extends AccessDB {
         try {
 
             String sql = "select * from libro";
-            ResultSet result = instruccion.executeQuery(sql);
-            System.out.println("Consultando.....");
-
-            while (result.next()) {
-                System.out.println("------------------------------");
-                System.out.println("Id:" + result.getInt(1));
-                System.out.println("Nombre Libro:" + result.getString(2));
-                System.out.println("ISBN:" + result.getString(3));
-                System.out.println("Categoria:" + result.getString(4));
-                System.out.println("------------------------------");
-                System.out.println("------------------------------");
-
+            try (ResultSet result = instruccion.executeQuery(sql)) {
+                System.out.println("Consultando.....");
+                
+                while (result.next()) {
+                    System.out.println("------------------------------");
+                    System.out.println("Id:" + result.getInt(1));
+                    System.out.println("Nombre Libro:" + result.getString(2));
+                    System.out.println("ISBN:" + result.getString(3));
+                    System.out.println("Categoria:" + result.getString(4));
+                    System.out.println("------------------------------");
+                    System.out.println("------------------------------");
+                    
+                }
+                
+                System.out.println("Competado!!!");
             }
-
-            System.out.println("Competado!!!");
-
-            result.close();
             instruccion.close();
             conexion.close();
 
@@ -100,7 +101,6 @@ public class Operaciones extends AccessDB {
      */
     public void insertNewBook() {
 
-        
         System.out.println(" Ingere el nombre del nuevo libro (obligatorio) ");
 
         String nombre = SC.nextLine();
@@ -136,9 +136,9 @@ public class Operaciones extends AccessDB {
         try {
             this.createConection();
             instruccion.executeUpdate(Query);
-            
+
         //    raw = instruccion.getUpdateCount();
-         //   System.out.println("Clomunas afectadas: "+raw);
+            //   System.out.println("Clomunas afectadas: "+raw);
             System.out.println("Datos insertados correctamente!!!");
 
             instruccion.close();
@@ -156,7 +156,6 @@ public class Operaciones extends AccessDB {
      */
     public void insertCliente() {
 
-        
         System.out.println(" Ingere el nombre del nuevo cliente ");
         String nombre = SC.nextLine();
         while (nombre.equals(null) | nombre.equals("")) {
@@ -189,7 +188,7 @@ public class Operaciones extends AccessDB {
             instruccion.executeUpdate(Query);
             System.out.println("Datos insertados correctamente!!!");
             raw = instruccion.getUpdateCount();
-         System.out.println("Clomunas afectadas: "+raw);
+            System.out.println("Clomunas afectadas: " + raw);
             instruccion.close();
             conexion.close();
         } catch (SQLException ex) {
@@ -199,99 +198,119 @@ public class Operaciones extends AccessDB {
 
     }
 //------------------------------------------------------------------------------------
+
     public void delete() throws SQLException {
-        
+
         String SQL = null;
-        int id ;
+        int id;
         System.out.println("Que desea eliminar ");
         System.out.println("1- Usuarios");
         System.out.println("2- Libros");
-       Integer respuesta= SC.nextInt();
+        Integer respuesta = SC.nextInt();
         while (respuesta.equals("") | respuesta > 2) {
-            System.out.println("Debe ingresar una opncion valida");           
-            
+            System.out.println("Debe ingresar una opncion valida");
+
         }
-        
+
         switch (respuesta) {
             case 1:
                 System.out.println("Ingrese el ID del usuario");
                 id = SC.nextInt();
-                SQL = "DELETE FROM persona WHERE Id_Persona = "+id;
+                SQL = "DELETE FROM persona WHERE Id_Persona = " + id;
                 try {
                     this.createConection();
                     instruccion.executeUpdate(SQL);
-                    
-                    
+
                 } catch (Exception e) {
                     System.out.println("No se puedo eliminar ningun usuario con ese Id");
                     e.printStackTrace();
-                }
-              finally {
+                } finally {
                     instruccion.close();
                     conexion.close();
-                break;
+                    break;
                 }
-                case 2:
+            case 2:
                 System.out.println("Ingrese el ID del libro");
                 id = SC.nextInt();
-                 SQL = "DELETE FROM libro WHERE Id_Libro = "+id;
+                SQL = "DELETE FROM libro WHERE Id_Libro = " + id;
                 try {
                     this.createConection();
-                   instruccion.executeUpdate(SQL);
-                    
-                    
+                    instruccion.executeUpdate(SQL);
+
                 } catch (Exception e) {
                     System.out.println("No se puedo eliminar ningun libro con ese ID");
                     e.printStackTrace();
-                }
-                finally {
+                } finally {
                     instruccion.close();
                     conexion.close();
 
                 }
-                
+
                 break;
             default:
                 System.out.println("No se eligio una opcion valida");
         }
-            
-        }
-            
-       public void addPrestamo(){
-           System.out.println("Que tipo de prestamo desea realizar?");
-           System.out.println("1- Prestamo para un cliente ya resgistrado");
-           System.out.println("2- Prestamo para un nuevo cliente");
-           String Respuesta = SC.nextLine();
-           switch (Respuesta) {
-               case "1":
-                   System.out.println("Ingrese el Id del cliente");
-                   int id_Cliente = SC.nextByte();
-                   System.out.println("Ingrese el Id del empleado");
-                   int id_Empleado = SC.nextInt();
-                   System.out.println("Ingrese el Id del libro");
-                   int id_Libro = SC.nextInt();
-                   String SQL = "INSERT INTO prestamo (id_empleado,id_cliente,id_libro) VALUES ('"+id_Empleado+"','"+id_Cliente+"','"+id_Libro+"')";
-                   try {
-                       this.createConection();
-                       this.instruccion.executeUpdate(SQL);
-                       
-                       System.out.println("Datos Ingresados Correctamente");
-                        raw = this.instruccion.getUpdateCount();
-                       System.out.println("Columnas afectadas: "+raw);
-                       
-                       
-                   } catch (Exception e) {
-                       System.out.println(" no se pudeo realizar la accion");
-                       e.printStackTrace();
-                   }
-                           
-                   
-                   break;
-               default:
-                   throw new AssertionError();
-           }
-       
-       }
-        
+
     }
 
+    public void addPrestamo() {
+        int id_Cliente, id_Emplead, id_Libro, id_Empleado;
+        String SQL = null;
+        System.out.println("Que tipo de prestamo desea realizar?");
+        System.out.println("1- Prestamo para un cliente ya resgistrado");
+        System.out.println("2- Prestamo para un nuevo cliente");
+        String Respuesta = SC.nextLine();
+        switch (Respuesta) {
+            case "1":
+                System.out.println("Ingrese el Id del cliente");
+                id_Cliente = SC.nextByte();
+                System.out.println("Ingrese el Id del empleado");
+                id_Empleado = SC.nextInt();
+                System.out.println("Ingrese el Id del libro");
+                id_Libro = SC.nextInt();
+                SQL = "INSERT INTO prestamo (id_empleado,id_cliente,id_libro) VALUES ('" + id_Empleado + "','" + id_Cliente + "','" + id_Libro + "')";
+                try {
+                    this.createConection();
+                    this.instruccion.executeUpdate(SQL);
+
+                    System.out.println("Datos Ingresados Correctamente");
+                    raw = this.instruccion.getUpdateCount();
+                    System.out.println("Columnas afectadas: " + raw);
+
+                } catch (Exception e) {
+                    System.out.println(" no se pudeo realizar la accion");
+                    e.printStackTrace();
+                }
+
+                break;
+            case "2":
+                insertCliente();
+                System.out.println("Ingrese el Id del cliente");
+                id_Cliente = SC.nextByte();
+                System.out.println("Ingrese el Id del empleado");
+                id_Empleado = SC.nextInt();
+                System.out.println("Ingrese el Id del libro");
+                id_Libro = SC.nextInt();
+                SQL = "INSERT INTO prestamo (id_empleado,id_cliente,id_libro) VALUES ('" + id_Empleado + "','" + id_Cliente + "','" + id_Libro + "')";
+
+                try {
+                    this.createConection();
+                    this.instruccion.executeUpdate(SQL);
+
+                    System.out.println("Datos Ingresados Correctamente");
+                    raw = this.instruccion.getUpdateCount();
+                    System.out.println("Columnas afectadas: " + raw);
+
+                } catch (Exception e) {
+                    System.out.println(" no se pudeo realizar la accion");
+                    e.printStackTrace();
+                }
+
+                break;
+            default:
+                throw new AssertionError();
+        }
+
+    }
+
+}
